c46cb8f966d5fc9c29ae14847a99b54cefe98cde
AWS Server <rrrhys+aws@gmail.com>
2013-12-05 09:47:44 +0000

    Save unsaved

--- app/controllers/WidgetController.php
+++ app/controllers/WidgetController.php
@@ -4,16 +4,24 @@ class WidgetController extends BaseController {
 
 	public function js($widget_token){
 		$user = User::where('widget_token','=',$widget_token)->first();
-		$widget_session = new WidgetSession();
-		$widget_session->token = str_random(16);
-		//echo $user->widget->id;
-		//die();
-		$user->widget->sessions()->save($widget_session);
 		if(!$user){
 			$response = Response::make("Not authorized", 401);
 			return $response;
 		}
 		else{
+
+			//load the session that came in with the cookie, otherwise make a new session.
+			$existing_widget_session_id_from_cookie = Cookie::get('widget_session_id');
+
+			$widget_session = $user->widget->sessions()->where('id','=',$existing_widget_session_id_from_cookie)->first();
+			if(!$widget_session){
+				$widget_session = new WidgetSession();
+				$widget_session->token = str_random(16);
+				$user->widget->sessions()->save($widget_session);
+				Cookie::queue('widget_session_id', $widget_session->id, 60);
+			}
+
+
 			$vars = array();
 			$contents = View::make('widget/init_js')->with(array(
 				'widget'=>$user->widget,
